ea3c2bf2fd6549fb44abca629439c2c4e4ee0017
Strongeric89 <ericstrong89@gmail.com>
2017-03-19 16:22:56 +0000

    phase 4: complete. minor bugs need fixing

--- app/src/main/java/com/eric/dt211c/dittimetableapp/ReadApp.java
+++ app/src/main/java/com/eric/dt211c/dittimetableapp/ReadApp.java
@@ -14,15 +14,22 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Scanner;
 
+
+/*read app designed by eric strong */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class ReadApp
 {
-    public static ArrayList<String[]> list = new ArrayList<String[]>();
-    public static HashMap<Integer,String[]> classes = new HashMap <Integer,String[]>();
-    public static HashMap<Integer, String[]> mapMonday = new HashMap<Integer, String[]>();
-    public static HashMap<Integer, String[]> mapTuesday = new HashMap<Integer, String[]>();
-    public static HashMap<Integer, String[]> mapWednesday = new HashMap<Integer, String[]>();
-    public static HashMap<Integer, String[]> mapThursday = new HashMap<Integer, String[]>();
-    public static HashMap<Integer, String[]> mapFriday = new HashMap<Integer, String[]>();
+    public static HashMap<Integer, String[]> classes = new HashMap<Integer, String[]>();
+    public static ArrayList<String> list = new ArrayList<String>();
 
     public String filename;
 
@@ -49,98 +56,115 @@ public class ReadApp
 
     }// end readFile
 
-
-
-    public HashMap<Integer,String[]> getContents() {
-
-        File file = new File(filename);
-        Scanner in;
+    public HashMap<Integer, String[]> getContents()
+    {
         int day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-        day = day - 1;
-
+        day = day + 1;
         // convert the number to a string to return the correct version
         String dayOfWeek = new Integer(day).toString();
-        Log.d("eric", "day value is  " + dayOfWeek);
-
-
-        try {
-            in = new Scanner(file);
-            String line;
-            String[] lineContents;
-
-
-            if (dayOfWeek.equalsIgnoreCase("1")) {
-
-                while ((line = in.nextLine()) != null && line.startsWith("1")) {
-
-                    lineContents = line.split(",");
-                    Integer key = Integer.parseInt(lineContents[1]);
-                    mapMonday.put(key, new String[]{lineContents[2], lineContents[3]});
-
-                }//end for
-                classes.putAll(mapMonday);
-            }
-
-            if (dayOfWeek.equalsIgnoreCase("2")) {
-
-                while ((line = in.nextLine()) != null && line.startsWith("2")) {
-                    lineContents = line.split(",");
-                    Integer key = Integer.parseInt(lineContents[1]);
-                    mapTuesday.put(key, new String[]{lineContents[2], lineContents[3]});
-
-                }//end for
-                classes.putAll(mapTuesday);
-            }
-
-            if (dayOfWeek.equalsIgnoreCase("3")) {
-
-                while ((line = in.nextLine()) != null && line.startsWith("3")) {
-                    lineContents = line.split(",");
-                    Integer key = Integer.parseInt(lineContents[1]);
-                    mapWednesday.put(key, new String[]{lineContents[2], lineContents[3]});
-
-                }//end for
-                classes.putAll(mapWednesday);
-            }
-
-            if (dayOfWeek.equalsIgnoreCase("4")) {
-
-                while ((line = in.nextLine()) != null && line.startsWith("4")) {
-                    lineContents = line.split(",");
-                    Integer key = Integer.parseInt(lineContents[1]);
-                    mapThursday.put(key, new String[]{lineContents[2], lineContents[3]});
-
-                }//end for
-                classes.putAll(mapThursday);
-            }
-
-            if (dayOfWeek.equalsIgnoreCase("5")) {
-
-                while ((line = in.nextLine()) != null && line.startsWith("5")) {
-                    lineContents = line.split(",");
-                    Integer key = Integer.parseInt(lineContents[1]);
-                    mapFriday.put(key, new String[]{lineContents[2], lineContents[3]});
-
-                }//end for
-                classes.putAll(mapFriday);
-
-            }
-
-
-//            while((line = in.nextLine()) !=null && line.startsWith(dayOfWeek)){
-//                lineContents = line.split(",");
-//                Integer key= Integer.parseInt(lineContents[1]);
-//                classes.put(key,new String[] {lineContents[2],lineContents[3]});
-        }
-         catch (FileNotFoundException e)
+        switch (day)
         {
-            System.out.println("file not found");
-        }
+
+            case 1:
+            {
+                sorter(dayOfWeek);
+
+            }
+            break;
+
+            case 2:
+            {
+                sorter(dayOfWeek);
+
+            }
+            break;
+
+            case 3:
+            {
+                sorter(dayOfWeek);
+
+            }
+            break;
+
+            case 4:
+            {
+                sorter(dayOfWeek);
+
+            }
+            break;
+
+            case 5:
+            {
+                sorter(dayOfWeek);
+
+            }
+            break;
+
+            case 6:
+            {
+                sorter(dayOfWeek);
+
+            }
+            break;
+
+            case 7:
+            {
+                sorter(dayOfWeek);
+
+            }
+            break;
+
+        }// end switch
 
         return classes;
-    }
+    }// end getContents
 
+    public void sorter(String dayOfWeek)
+    {
 
+        File file = new File(filename);
+
+        Scanner in;
+        try
+        {
+            in = new Scanner(file);
+
+            String line;
+            while (in.hasNext())
+            {
+                line = in.next();
+                if (line.startsWith(dayOfWeek))
+                {
+                    // added to arraylist
+                    list.add(line);
+
+                } // end getting lines
+
+            } // reading in the file
+
+            String values[] = new String[4];
+
+            for (String s : list)
+            {
+                values = s.split(",");
+                for (int j = 0; j < values.length; j++)
+                {
+                    // IMPORTANT PART TO POPULATE THE HASHMAP
+                    Integer key = Integer.parseInt(values[1]);
+                    classes.put(key, new String[]
+                            { values[2], values[3] });
+
+                } // end inner
+
+            } // end enhanced for
+
+        } catch (FileNotFoundException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }// end sorter
 
     public void writeFile(String outputfile, String argument) throws IOException
     {
