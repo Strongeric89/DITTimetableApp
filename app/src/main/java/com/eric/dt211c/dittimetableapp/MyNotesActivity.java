257afa9cf0cc118cb3d3d2764a3a633f855058e4
Strongeric89 <ericstrong89@gmail.com>
2017-03-19 21:14:02 +0000

    phase 6: complete my notes and view notes complete. some minor bugs but now only need to make dialog box fields fill with text fields from myNotes. also added images for every day of  the week to change dynamically

--- app/src/main/java/com/eric/dt211c/dittimetableapp/MyNotesActivity.java
+++ app/src/main/java/com/eric/dt211c/dittimetableapp/MyNotesActivity.java
@@ -2,12 +2,70 @@ package com.eric.dt211c.dittimetableapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MyNotesActivity extends AppCompatActivity {
+
+    public static ArrayList<Task> taskList = new ArrayList<>();
+
+    private EditText mTitleField;
+    private EditText mDescField;
+    private Spinner mUgencyField;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notes);
-    }
-}
+
+        //Identifing the Id's from the activity_form xml
+        mTitleField = (EditText)findViewById(R.id.TaskNameField2);
+        mDescField = (EditText) findViewById(R.id.TaskDesField2);
+        mUgencyField = (Spinner) findViewById(R.id.TaskSpinner);
+
+    }//end onCreate
+
+
+    public void onSubmitPressed(View view){
+        String title = mTitleField.getText().toString();
+        String description = mDescField.getText().toString();
+        String urgency = mUgencyField.getSelectedItem().toString();
+
+        if(title.equals("") || description.equals("")){
+            Toast.makeText(this, "Please input all information",Toast.LENGTH_LONG).show();
+
+            return;
+        }// end if
+
+        int urgencyLevel = 0;
+        switch(urgency){
+            case "High" : urgencyLevel = 3;break;
+            case "Medium" : urgencyLevel =2;break;
+            case "Low": urgencyLevel = 1;break;
+        }
+
+
+
+        Task task = new Task(title, description,urgencyLevel);
+        taskList.add(task);
+
+        Toast.makeText(this,"Note Added",
+                Toast.LENGTH_LONG).show();
+
+
+        //clear the screen
+        mTitleField.setText("");
+        mDescField.setText("");
+        mUgencyField.setSelection(0);
+
+    }//end on submit pressed
+
+
+
+
+}//end class
--- app/src/main/java/com/eric/dt211c/dittimetableapp/ViewNotesActivity.java
+++ app/src/main/java/com/eric/dt211c/dittimetableapp/ViewNotesActivity.java
@@ -1,7 +1,15 @@
 package com.eric.dt211c.dittimetableapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ViewNotesActivity extends AppCompatActivity {
 
@@ -10,5 +18,68 @@ public class ViewNotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_view_notes);
+
+        //creating the link to xml
+        ListView taskListView =(ListView) findViewById(R.id.task_list);
+
+        String [] taskTitles = new String[MyNotesActivity.taskList.size()];  //goes to the MyNotesActivity,java and gets the size of the array list
+
+        //populate the array
+        for(int i=0;i<taskTitles.length;i++){
+            taskTitles[i]=MyNotesActivity.taskList.get(i).getTitle(); //getting the title part from the list of index i
+
+        }//end for
+
+        //setting up arrayAdapter whichi handles the contents of the ListView
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, taskTitles);
+
+        taskListView.setAdapter(adapter);
+
+        //Implementing the listener for pressing on a long click on an item
+        taskListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long l) {
+                MyNotesActivity.taskList.remove(i);
+                recreate();
+                return false;
+            }
+        });
+
+
+
+    }//end on create
+
+    public void FormActivity (View view){
+        Intent intent = new Intent(this,ViewNotesActivity.class);
+        createDialogIntent(this, intent);
     }
-}
+
+
+    //for additional info on the charities
+    public static void createDialogIntent(final Context context, final Intent intent) {
+
+        String title;
+        String desription;
+        String priority;
+
+
+
+        new AlertDialog.Builder(context).setTitle(R.string.alert_title)
+                .setMessage(R.string.alert_message)
+                .setIcon(R.drawable.classroom3)
+                .setPositiveButton(R.string.alert_agree, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //this is what happens when Accept is clicked
+                        //context.startActivity(intent);
+                    }
+                }).show();
+
+    }
+
+
+
+
+
+
+}//end class
